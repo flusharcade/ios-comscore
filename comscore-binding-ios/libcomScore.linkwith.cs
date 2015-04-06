2403843e73c0406f353938d4952e94e3a2f39152
Michael Williams <info@flusharcade.com>
2015-04-07 09:23:04 +1000

    added Link Targets for armV7 and 64 bitsimulator

--- comscore-binding-ios/libcomScore.linkwith.cs
+++ comscore-binding-ios/libcomScore.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libcomScore.a", LinkTarget.Arm64 | LinkTarget.Simulator, Frameworks="SystemConfiguration Security", SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libcomScore.a", LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks="SystemConfiguration Security", SmartLink = true, ForceLoad = true)]
